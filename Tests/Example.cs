c6928ed4cba74e77a83ce3beb6e82c28eac0d28f
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-23 00:43:18 -0300

    📝 add example test

---
+++ Tests/Example.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void IsTrue ()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
