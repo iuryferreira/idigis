f2026864a9b79da37a0d9c4653cbd2455584dd3f
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-23 19:17:55 -0300

    ✨ create crendentials value object

---
+++ Core/Domain/ValueObjects/Credentials.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using Shared.ValueObjects;
+
+namespace Domain.ValueObjects
+{
+    internal class Credentials : ValueObject
+    {
+        public string Email { get; private set; }
+        public string Password { get; internal set; }
+
+        public Credentials (string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+
+    internal class CredentialsValidator : AbstractValidator<Credentials>
+    {
+        public CredentialsValidator ()
+        {
+            RuleFor(credential => credential.Email).NotNull().NotEmpty().EmailAddress();
+            RuleFor(credential => credential.Password).NotNull().NotEmpty().MinimumLength(8);
+        }
+    }
+}
