3b1e32b4060358f70ab1da47c7fca1e11e5b2b61
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-24 02:15:12 -0300

    ✨ create CreateChurch request

---
+++ Core/Application/Requests/CreateChurch.cs
@@ -0,0 +1,19 @@
+using Application.Responses;
+using MediatR;
+
+namespace Application.Requests
+{
+    public class CreateChurch : IRequest<CreateChurchResponse>
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string Password { get; }
+
+        public CreateChurch (string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
