88545c4dcb4f566fa0e110b39f141a2db4dfeb53
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-24 03:06:09 -0300

    ♻️ change set prop to init

--- Core/Application/Responses/CreateChurchResponse.cs
+++ Core/Application/Responses/CreateChurchResponse.cs
@@ -2,8 +2,8 @@ namespace Application.Responses
 {
     public class CreateChurchResponse
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Email { get; init; }
     }
 }
