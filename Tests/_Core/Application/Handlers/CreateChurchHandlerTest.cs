da261ef471719f446939346bf03e3c4f65ab5a4f
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-24 03:03:32 -0300

    🎨 fix namespace and optimize code

--- Tests/_Core/Application/Handlers/CreateChurchHandlerTest.cs
+++ Tests/_Core/Application/Handlers/CreateChurchHandlerTest.cs
@@ -1,12 +1,10 @@
-using System.Threading;
 using System.Threading.Tasks;
 using Application.Handlers;
 using Application.Requests;
 using Application.Responses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Shared.Notifications;
 
-namespace Tests._Core.Application
+namespace Tests._Core.Application.Handlers
 {
     [TestClass]
     public class CreateChurchHandlerTest
@@ -14,18 +12,18 @@ namespace Tests._Core.Application
         [TestMethod]
         public async Task Should_Return_Null_If_the_Entity_Is_Invalid ()
         {
-            CreateChurchHandler sut = new(new NotificationContext());
+            CreateChurchHandler sut = new(new());
             CreateChurch data = new("", "", "");
-            var result = await sut.Handle(data, new CancellationToken());
+            var result = await sut.Handle(data, new());
             Assert.IsNull(result);
         }
 
         [TestMethod]
         public async Task Should_Return_Response_If_the_Entity_Is_Valid ()
         {
-            CreateChurchHandler sut = new(new NotificationContext());
+            CreateChurchHandler sut = new(new());
             CreateChurch data = new("valid_name", "valid_email@email.com", "valid_password");
-            var result = await sut.Handle(data, new CancellationToken());
+            var result = await sut.Handle(data, new());
             Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(CreateChurchResponse));
         }
