58e51e0209855aff955f2fb8725883a3fc0cedb0
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-24 02:16:23 -0300

    ✅ create tests for notification context

---
+++ Tests/_Core/Shared/Notifications/NotificationContextTest.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Notifications;
+
+namespace Tests._Core.Application
+{
+    [TestClass]
+    public class NotificationContextTest
+    {
+        [TestMethod]
+        public void Should_Return_True_If_There_Is_Any_Notification ()
+        {
+            NotificationContext sut = new();
+            sut.AddNotification("valid_key", "valid_message");
+            Assert.IsTrue(sut.HasNotifications);
+        }
+
+        [TestMethod]
+        public void Should_Return_a_List_of_Notifications_Greater_than_Zero ()
+        {
+            NotificationContext sut = new();
+            sut.AddNotification("valid_key", "valid_message");
+            Assert.AreEqual(1, sut.Notifications.Count);
+        }
+        [TestMethod]
+        public void Should_Return_a_Valid_Notification ()
+        {
+            NotificationContext sut = new();
+            sut.AddNotification("valid_key", "valid_message");
+            Assert.AreEqual("valid_key", sut.Notifications.Single().Key);
+            Assert.AreEqual("valid_message", sut.Notifications.Single().Message);
+        }
+    }
+}
