bafd6b4d65079f55da9eeacfaaeb15c8b9771bb9
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-24 02:15:29 -0300

    ✨ create CreateChurchResponse

---
+++ Core/Application/Responses/CreateChurchResponse.cs
@@ -0,0 +1,9 @@
+namespace Application.Responses
+{
+    public class CreateChurchResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
