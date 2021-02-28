ece7689341947de3377c48833ec8aa48a28d67e9
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-28 00:15:39 -0300

    ✨ add env type helper

---
+++ Core/Shared/Helpers/EnvType.cs
@@ -0,0 +1,9 @@
+namespace Shared.Helpers
+{
+    public static class EnvType
+    {
+        public const string Development = "Development";
+        public const string Production = "Production";
+        public const string Testing = "Testing";
+    }
+}
