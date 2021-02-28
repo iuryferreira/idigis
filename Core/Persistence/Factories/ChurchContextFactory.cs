322a63d55e238310f61dd882d17d3466bb0fbb2c
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-28 02:56:14 -0300

    ♻️ add factory to codecoverage

--- Core/Persistence/Factories/ChurchContextFactory.cs
+++ Core/Persistence/Factories/ChurchContextFactory.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
@@ -6,25 +8,34 @@ using Persistence.Contexts;
 
 namespace Persistence.Factories
 {
+#nullable enable
     public class ChurchContextFactory : IDesignTimeDbContextFactory<ChurchContext>
     {
+        [ExcludeFromCodeCoverage]
         public ChurchContext CreateDbContext (string[] args)
         {
-            return CreateDbContext();
+            return GenerateDbContext();
         }
-
-        private static ChurchContext CreateDbContext ()
+        public static ChurchContext CreateDbContext (string? connectionString = null)
         {
-            var configuration = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddUserSecrets<ChurchContextFactory>()
-               .Build();
-
-            var optionsBuilder = new DbContextOptionsBuilder<ChurchContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("default"));
-
-            return new ChurchContext(optionsBuilder.Options);
+            return GenerateDbContext(connectionString);
         }
+        private static ChurchContext GenerateDbContext (string? connectionString = null)
+        {
+            var env = Environment.GetEnvironmentVariable("DOTNET_ENV");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets<ChurchContextFactory>()
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<ChurchContext>();
+            if (connectionString is null)
+            {
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("default"));
+            }
+            else
+                optionsBuilder.UseSqlServer(connectionString);
 
+            return new(optionsBuilder.Options);
+        }
     }
 }
