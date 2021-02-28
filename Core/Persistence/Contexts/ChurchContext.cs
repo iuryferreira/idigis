80769a9ed43e8573cbd9dbc33d2bcdf7b9fe9aa0
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-02-28 02:55:44 -0300

    ✨ implements interface

--- Core/Persistence/Contexts/ChurchContext.cs
+++ Core/Persistence/Contexts/ChurchContext.cs
@@ -1,20 +1,42 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Persistence.Contracts;
 using Persistence.Models;
 
 namespace Persistence.Contexts
 {
-    public class ChurchContext : DbContext
+    public class ChurchContext : DbContext, IChurchContext
     {
-        public ChurchContext (DbContextOptions<ChurchContext> options) : base(options) { }
+        public ChurchContext (DbContextOptions<ChurchContext> options) : base(options)
+        {
+            if (!this.Database.CanConnect())
+                throw new Exception("Could not connect to the database.");
+        }
 
-        public DbSet<ChurchModel> Churches { get; set; }
+        private DbSet<ChurchModel> Entity { get; set; }
+
+        public async Task<bool> Add (ChurchModel data)
+        {
+            var entry = await Entity.AddAsync(data);
+            await this.SaveChangesAsync();
+            return entry.State is EntityState.Unchanged;
+        }
+
+        public async Task<bool> Exists (ChurchModel data)
+        {
+            ChurchModel entity = await Entity.Where(e => e.Email == data.Email || e.Id == data.Id).FirstOrDefaultAsync();
+            return entity is not null;
+        }
 
         protected override void OnModelCreating (ModelBuilder builder)
         {
             builder.ApplyConfiguration(new ChurchTypeConfiguration());
         }
     }
+
     internal class ChurchTypeConfiguration : IEntityTypeConfiguration<ChurchModel>
     {
         public void Configure (EntityTypeBuilder<ChurchModel> builder)
@@ -22,5 +44,4 @@ namespace Persistence.Contexts
             builder.ToTable("churches");
         }
     }
-
 }
