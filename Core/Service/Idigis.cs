381a20c2dde8435abeb6b7589b7377c9ddbde3eb
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-05 03:19:44 -0300

    ✨ create idigis service

---
+++ Core/Service/Idigis.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Service
+{
+    [ExcludeFromCodeCoverage]
+    public static class Idigis
+    {
+        public static void Add (this IServiceCollection services, string connectionString)
+        {
+            Shared.Register.AddLayer(services);
+            Persistence.Register.AddLayer(services, connectionString);
+            Application.Register.AddLayer(services);
+        }
+    }
+}
