9dc13a36ce12718cd0560094827657e36018b7c3
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-05 01:03:58 -0300

    ✨ create interface for  create church handler

---
+++ Core/Application/Handlers/ICreateChurchHandler.cs
@@ -0,0 +1,8 @@
+using Application.Requests;
+using Application.Responses;
+using MediatR;
+
+namespace Application.Handlers
+{
+    public interface ICreateChurchHandler : IRequestHandler<CreateChurch, CreateChurchResponse> { }
+}
