50666b7c85455aaf99d510bd263bda1a930add72
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-05 01:07:28 -0300

    🔥 remove handler class

--- Core/Shared/Handlers/Handler.cs
+++
@@ -1,9 +0,0 @@
-using Shared.Notifications;
-
-namespace Shared.Handlers
-{
-    public abstract class Handler : Notifiable
-    {
-        protected Handler (NotificationContext notificationContext) : base(notificationContext) { }
-    }
-}
