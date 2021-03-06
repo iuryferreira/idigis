ba19ba535065ae9e511e2c9f2226e422326ca656
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-06 03:11:40 -0300

    🔥 remove unused method

--- Core/Shared/Notifications/INotificator.cs
+++ Core/Shared/Notifications/INotificator.cs
@@ -7,7 +7,6 @@ namespace Shared.Notifications
     {
         IReadOnlyCollection<Notification> Notifications { get; }
         bool HasNotifications { get; }
-        void AddNotification (string key, string message);
         void AddNotifications (ValidationResult validationResult);
         void AddNotifications (IEnumerable<Notification> notifications);
     }
