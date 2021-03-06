3b450188e1acbb847691f42ddb8c0fc5da66aae0
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-06 03:10:40 -0300

    🎨 improve code style

--- Core/Persistence/Register.cs
+++ Core/Persistence/Register.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
-using Domain.Contracts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Contexts;
 using Persistence.Contracts;
@@ -11,19 +11,13 @@ namespace Persistence
     [ExcludeFromCodeCoverage]
     public static class Register
     {
-        public static void AddLayer (this IServiceCollection services, string connectionString)
-        {
-            services.AddPersistence(connectionString);
-        }
-
-        private static void AddPersistence (this IServiceCollection services, string connectionString)
+        public static void AddPersistence (this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IChurchRepository, ChurchRepository>();
             services.AddDbContext<IChurchContext, ChurchContext>(options =>
             {
-                options.UseSqlServer(connectionString);
+                options.UseSqlServer(configuration.GetConnectionString("Default"));
             });
-
         }
     }
 }
