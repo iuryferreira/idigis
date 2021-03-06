dec1b1cb9eb4e1ccf3c2f09c589e3eb7f653e96e
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-06 03:13:53 -0300

    🔥 remove unused class

--- Core/Shared/Helpers/EnvType.cs
+++
@@ -1,9 +0,0 @@
-namespace Shared.Helpers
-{
-    public static class EnvType
-    {
-        public const string Development = "Development";
-        public const string Production = "Production";
-        public const string Testing = "Testing";
-    }
-}
