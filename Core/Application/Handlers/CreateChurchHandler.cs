3151033dee6d3f860eec240bec3436bb9a34e4ae
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-06 03:07:36 -0300

    ♻️ turn on notificator on public and update imports

--- Core/Application/Handlers/CreateChurchHandler.cs
+++ Core/Application/Handlers/CreateChurchHandler.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Requests;
 using Application.Responses;
-using Domain.Contracts;
 using Domain.Entities;
+using Persistence.Contracts;
 using Shared.Notifications;
 
 namespace Application.Handlers
@@ -11,28 +12,32 @@ namespace Application.Handlers
     public class CreateChurchHandler : ICreateChurchHandler
     {
         private readonly IChurchRepository _repository;
-        private readonly INotificator _notificator;
-        public INotificator Notificator => _notificator;
 
         public CreateChurchHandler (INotificator notificator, IChurchRepository repository)
         {
-            _notificator = notificator;
+            Notificator = notificator;
             _repository = repository;
         }
 
+        public INotificator Notificator { get; }
+
         public async Task<CreateChurchResponse> Handle (CreateChurch request, CancellationToken cancellationToken)
         {
+            Console.WriteLine(request.Password);
             Church entity = new(request.Name, new(request.Email, request.Password));
+
             if (entity.Invalid)
             {
-                _notificator.AddNotifications(entity.ValidationResult);
+                Notificator.AddNotifications(entity.ValidationResult);
                 return null;
             }
+
             if (!await _repository.Add(entity))
             {
-                _notificator.AddNotifications(_repository.Notifications);
+                Notificator.AddNotifications(_repository.Notifications);
                 return null;
             }
+
             var response =
                 new CreateChurchResponse { Id = entity.Id, Name = entity.Name, Email = entity.Credentials.Email };
             return response;
