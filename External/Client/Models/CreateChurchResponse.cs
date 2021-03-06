5c43c6a8632965ad42c506971eaed09b18db05d7
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-06 19:16:57 -0300

    ✨ add create church models

---
+++ External/Client/Models/CreateChurchResponse.cs
@@ -0,0 +1,9 @@
+namespace Client.Models
+{
+    public class CreateChurchResponse
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Email { get; init; }
+    }
+}
