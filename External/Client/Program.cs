e9e4f835a1f8b9ef6a46d8c76ebdbcf139ced414
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-07 13:19:58 -0300

    ✨ add localstorage and configure host

--- External/Client/Program.cs
+++ External/Client/Program.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,9 +12,8 @@ namespace Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+            builder.Services.AddScoped(services => new HttpClient { BaseAddress = new("https://localhost:5001") });
             await builder.Build().RunAsync();
         }
     }
