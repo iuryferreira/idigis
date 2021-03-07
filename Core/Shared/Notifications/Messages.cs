619aa3e59f9a161620e58c96489bf0c330ecaee6
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-07 15:58:46 -0300

    ♻️ update mininum condition

--- Core/Shared/Notifications/Messages.cs
+++ Core/Shared/Notifications/Messages.cs
@@ -6,10 +6,10 @@ namespace Shared.Notifications
         public const string NotEmpty = Base + " deve ser informado(a).";
         public const string Email = "Forneça um email válido.";
 
-        public static string Minimum (int size, string type = "string")
+        public static string Minimum (int size, bool isString = true)
         {
             var message = $"{Base} deve ser maior que {size}";
-            return type is "string" ? $"{message} caracteres." : $"{size}.";
+            return isString ? $"{message} caracteres." : $"{size}.";
         }
     }
 }
