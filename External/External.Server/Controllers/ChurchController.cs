2c0b41916d0cc5941684afa813e57af25fc13e00
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-08 16:14:35 -0300

    🚚 rename server project and update imports

---
+++ External/External.Server/Controllers/ChurchController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Core.Application.Requests;
+using Core.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace External.Server.Controllers
+{
+    [Route("api/churches")]
+    [ApiController]
+    public class ChurchController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ChurchController (IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CreateChurchResponse>> Store ([FromBody] CreateChurch request)
+        {
+            Console.WriteLine(request.Password);
+            var response = await _mediator.Send(request);
+            if (response is not null)
+            {
+                return Created($"{Request.GetDisplayUrl()}/{response.Id}", response);
+            }
+
+            return BadRequest();
+        }
+    }
+}
