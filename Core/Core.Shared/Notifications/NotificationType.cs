adeefdb6dbf388a67865cf4010d1ac7b4edb8479
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-10 14:39:46 -0300

    ✨ create notification type class

---
+++ Core/Core.Shared/Notifications/NotificationType.cs
@@ -0,0 +1,10 @@
+namespace Core.Shared.Notifications
+{
+    public enum NotificationType
+    {
+        Validation = 1,
+        Authentication = 2,
+        NotFound = 3,
+        Internal = 4
+    }
+}
