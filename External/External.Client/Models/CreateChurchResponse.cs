cfcf6e4003f487987263147d33142559d549aa6b
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-11 01:08:23 -0300

    🔥 remove unused fields

--- External/External.Client/Models/CreateChurchResponse.cs
+++ External/External.Client/Models/CreateChurchResponse.cs
@@ -2,8 +2,6 @@ namespace External.Client.Models
 {
     public class CreateChurchResponse
     {
-        public string Id { get; init; }
-        public string Name { get; init; }
         public string Email { get; init; }
     }
 }
