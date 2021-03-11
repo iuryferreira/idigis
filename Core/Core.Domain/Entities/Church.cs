d24b494c71436a2e6dcad50be69761895030775b
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-11 13:24:18 -0300

    ✨ create add offer method

--- Core/Core.Domain/Entities/Church.cs
+++ Core/Core.Domain/Entities/Church.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Core.Domain.ValueObjects;
 using Core.Shared.Entities;
+using Core.Domain.Aggregates;
 using Core.Shared.Notifications;
 using FluentValidation;
 using Hash;
@@ -8,10 +10,13 @@ namespace Core.Domain.Entities
 {
     public class Church : Entity
     {
+        private readonly List<Offer> _offers;
+
         public Church (string name, Credentials credentials)
         {
             Name = name;
             Credentials = credentials;
+            _offers = new List<Offer>();
             if (Validate(this, new ChurchValidator()))
             {
                 Credentials.Password = new Hashio().Hash(Credentials.Password);
@@ -25,9 +30,24 @@ namespace Core.Domain.Entities
             Credentials = credentials;
         }
 
+        public Church (string id, string name, Credentials credentials, List<Offer> offers)
+        {
+            Id = id;
+            Name = name;
+            Credentials = credentials;
+            _offers = offers;
+        }
+
+        public void AddOffer (Offer offer)
+        {
+            _offers.Add(offer);
+        }
+
         public string Name { get; }
 
         public Credentials Credentials { get; }
+
+        public IReadOnlyCollection<Offer> Offers => _offers;
     }
 
     internal class ChurchValidator : AbstractValidator<Church>
