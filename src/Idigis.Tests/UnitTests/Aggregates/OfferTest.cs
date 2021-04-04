d9568df4837e121df0c2f374b209b51639625066
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-04 16:56:54 -0300

    ✅ update tests

--- src/Idigis.Tests/UnitTests/Aggregates/OfferTest.cs
+++ src/Idigis.Tests/UnitTests/Aggregates/OfferTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Idigis.Core.Domain.Aggregates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,6 +12,15 @@ namespace Idigis.Tests.UnitTests.Aggregates
         {
             var sut = new Offer(0);
             Assert.IsTrue(sut.Invalid);
+        }
+
+        [TestMethod]
+        public void Entity_Should_Return_Error_Messages_if_Receive_a_Invalid_Data_in_Creation ()
+        {
+            var sut = new Offer(0);
+            var messages = sut.ValidationResult.Errors.Select(e => $"{e.PropertyName} - {e.ErrorMessage}").ToArray();
+            Assert.AreEqual(1, messages.Length);
+            Assert.AreEqual(messages[0], "Value - Este campo deve ser maior que 0.");
         }
 
         [TestMethod]
--- src/Idigis.Tests/UnitTests/Entities/ChurchTest.cs
+++ src/Idigis.Tests/UnitTests/Entities/ChurchTest.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Idigis.Core.Domain.Aggregates;
 using Idigis.Core.Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,6 +16,19 @@ namespace Idigis.Tests.UnitTests.Entities
         }
 
         [TestMethod]
+        public void Entity_Should_Return_Error_Messages_if_Receive_a_Invalid_Data_in_Creation ()
+        {
+            Church sut = new("", new("", ""));
+            var messages = sut.ValidationResult.Errors.Select(e => $"{e.PropertyName} - {e.ErrorMessage}").ToArray();
+            Assert.AreEqual(5, messages.Length);
+            Assert.AreEqual(messages[0], "Credentials.Email - Este campo deve ser informado(a).");
+            Assert.AreEqual(messages[1], "Credentials.Email - Forneça um email válido.");
+            Assert.AreEqual(messages[2], "Credentials.Password - Este campo deve ser informado(a).");
+            Assert.AreEqual(messages[3], "Credentials.Password - Este campo deve ser maior que 8 caracteres.");
+            Assert.AreEqual(messages[4], "Name - Este campo deve ser informado(a).");
+        }
+
+        [TestMethod]
         public void Entity_Must_Be_Valid_if_Receive_a_Valid_Data_in_Creation ()
         {
             Church sut = new("valid_name", new("valid@email.com", "valid_password"));
@@ -21,11 +36,27 @@ namespace Idigis.Tests.UnitTests.Entities
         }
 
         [TestMethod]
-        public void Must_Return_False_If_the_Creation_of_Offers_Is_Invalid ()
+        public void Must_Return_an_Offer_When_Receiving_the_Data ()
         {
             Church sut = new("valid_name", new("valid@email.com", "valid_password"));
             var offer = sut.AddOffer(0);
-            Assert.IsTrue(offer.Invalid);
+            Assert.IsInstanceOfType(offer, typeof(Offer));
+        }
+
+        [TestMethod]
+        public void Must_Not_Add_an_Offer_When_It_Is_Invalid ()
+        {
+            Church sut = new("valid_name", new("valid@email.com", "valid_password"));
+            sut.AddOffer(0);
+            Assert.AreEqual(0, sut.Offers.Count);
+        }
+
+        [TestMethod]
+        public void Must_Add_an_Offer_When_It_Is_Valid ()
+        {
+            Church sut = new("valid_name", new("valid@email.com", "valid_password"));
+            sut.AddOffer(1);
+            Assert.AreEqual(1, sut.Offers.Count);
         }
     }
 }
