436ff48c9c6cbfe2fb624f8fd03f64f817854494
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-04 16:33:10 -0300

    ✨  create add offer method

--- src/Idigis.Core/Domain/Entities/Church.cs
+++ src/Idigis.Core/Domain/Entities/Church.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using FluentValidation;
 using Hash;
+using Idigis.Core.Domain.Aggregates;
 using Idigis.Core.Domain.Contracts;
 using Idigis.Core.Domain.Helpers;
 using Idigis.Core.Domain.ValueObjects;
@@ -8,10 +10,13 @@ namespace Idigis.Core.Domain.Entities
 {
     internal class Church : Entity
     {
+        private readonly List<Offer> _offers;
+
         internal Church (string name, Credentials credentials)
         {
             Name = name;
             Credentials = credentials;
+            _offers = new();
             if (Validate(this, new ChurchValidator()))
             {
                 Credentials.Password = new Hashio().Hash(Credentials.Password);
@@ -20,6 +25,18 @@ namespace Idigis.Core.Domain.Entities
 
         private string Name { get; }
         private Credentials Credentials { get; }
+        public IReadOnlyCollection<Offer> Offers => _offers;
+
+        internal Offer AddOffer (decimal value)
+        {
+            var offer = new Offer(value);
+            if (!offer.Invalid)
+            {
+                _offers.Add(offer);
+            }
+
+            return offer;
+        }
 
         private class ChurchValidator : AbstractValidator<Church>
         {
