2d891fcdbed5a518fa6ed962239ec4fcb45a3e2c
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-06 19:10:07 -0300

    ✨ add notificator to usecase contract

--- src/Idigis.Core/Application/Contracts/IChurchUseCase.cs
+++ src/Idigis.Core/Application/Contracts/IChurchUseCase.cs
@@ -1,11 +1,14 @@
 using System.Threading.Tasks;
 using Idigis.Core.Domain.Dtos.Requests;
 using Idigis.Core.Domain.Dtos.Responses;
+using Notie.Contracts;
 
 namespace Idigis.Core.Application
 {
     public interface IChurchUseCase
     {
+        AbstractNotificator Notificator { get; }
+
         public Task<CreateChurchResponse> Add (CreateChurchRequest data);
     }
 }
