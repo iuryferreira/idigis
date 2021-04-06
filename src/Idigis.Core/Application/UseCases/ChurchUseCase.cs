b010ce83bb6f8d7e7a4e5703911a36149ec932dc
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-06 19:40:39 -0300

    ✨ create church use case

---
+++ src/Idigis.Core/Application/UseCases/ChurchUseCase.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Idigis.Core.Application.Contracts;
+using Idigis.Core.Domain.Entities;
+using Idigis.Core.Dtos.Requests;
+using Idigis.Core.Dtos.Responses;
+using Idigis.Core.Persistence.Contracts;
+using Notie.Contracts;
+
+namespace Idigis.Core.Application.UseCases
+{
+    internal class ChurchUseCase : IChurchUseCase
+    {
+        private readonly IChurchRepository _repository;
+
+        public ChurchUseCase (AbstractNotificator notificator, IChurchRepository repository)
+        {
+            Notificator = notificator;
+            _repository = repository;
+        }
+
+        public AbstractNotificator Notificator { get; }
+
+        public async Task<CreateChurchResponse> Add (CreateChurchRequest data)
+        {
+            var entity = new Church(data.Name, new(data.Email, data.Password));
+            if (entity.Invalid)
+            {
+                Notificator.AddNotificationsByFluent(entity.ValidationResult);
+                return null;
+            }
+
+            if (!await _repository.Add(entity))
+            {
+                return null;
+            }
+
+            return new() { Id = entity.Id, Name = entity.Name, Email = entity.Credentials.Email };
+        }
+    }
+}
