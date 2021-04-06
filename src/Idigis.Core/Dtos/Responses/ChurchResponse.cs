df0bf6a97e801e833cf6595629c95557ba6eaff2
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-06 19:10:25 -0300

    ✨ create response dto

---
+++ src/Idigis.Core/Dtos/Responses/ChurchResponse.cs
@@ -0,0 +1,9 @@
+namespace Idigis.Core.Domain.Dtos.Responses
+{
+    public class CreateChurchResponse
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Email { get; init; }
+    }
+}
