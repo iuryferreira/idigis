0d3c8958e1d950de40a4ea538bd5e809b71f6e46
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-06 21:34:42 -0300

    ✅ update tests

--- src/Idigis.Tests/UnitTests/UseCases/ChurchUseCaseTest.cs
+++ src/Idigis.Tests/UnitTests/UseCases/ChurchUseCaseTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Idigis.Core.Application.Contracts;
@@ -16,17 +17,12 @@ namespace Idigis.Tests.UnitTests.UseCases
     [TestClass]
     public class ChurchUseCaseTest
     {
-        private readonly CreateChurchRequest _invalidData;
         private readonly Mock<IChurchRepository> _repository;
-        private readonly CreateChurchRequest _validData;
         private AbstractNotificator _notificator;
         private IChurchUseCase _sut;
 
-
         public ChurchUseCaseTest ()
         {
-            _invalidData = new("", "", "");
-            _validData = new("valid_name", "valid_email@email.com", "valid_password");
             _repository = new();
         }
 
@@ -38,15 +34,15 @@ namespace Idigis.Tests.UnitTests.UseCases
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Data_Provided_Is_Invalid ()
+        public async Task Must_Return_Null_if_Data_Provided_To_Create_Is_Invalid ()
         {
-            Assert.IsNull(await _sut.Add(_invalidData));
+            Assert.IsNull(await _sut.Add(new("", "", "")));
         }
 
         [TestMethod]
-        public async Task Must_Add_Notifications_if_Data_Provided_Is_Invalid ()
+        public async Task Must_Add_Notifications_if_Data_Provided_To_Create_Is_Invalid ()
         {
-            await _sut.Add(_invalidData);
+            await _sut.Add(new("", "", ""));
             var messages = _sut.Notificator.Notifications
                 .Select(notification => $"{notification.Key} - {notification.Message}").ToArray();
             Assert.IsTrue(_sut.Notificator.HasNotifications);
@@ -59,21 +55,95 @@ namespace Idigis.Tests.UnitTests.UseCases
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Entity_is_Not_Persisted ()
+        public async Task Must_Return_Null_if_Entity_is_Not_Persisted_In_Add_Method ()
         {
             _repository.Setup(repository => repository.Add(It.IsAny<Church>())).ReturnsAsync(false);
-            Assert.IsNull(await _sut.Add(_validData));
+            Assert.IsNull(await _sut.Add(new("valid_name", "valid_email@email.com", "valid_password")));
         }
 
         [TestMethod]
-        public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted ()
+        public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted_In_Add_Method ()
         {
             _repository.Setup(repository => repository.Add(It.IsAny<Church>())).ReturnsAsync(true);
-            var response = await _sut.Add(_validData);
+            var data = new CreateChurchRequest("valid_name", "valid_email@email.com", "valid_password");
+            var response = await _sut.Add(data);
             Assert.IsInstanceOfType(response, typeof(CreateChurchResponse));
             Assert.IsNotNull(response.Id);
-            Assert.AreEqual(_validData.Name, response.Name);
-            Assert.AreEqual(_validData.Email, response.Email);
+            Assert.AreEqual(data.Name, response.Name);
+            Assert.AreEqual(data.Email, response.Email);
+        }
+
+        [TestMethod]
+        public async Task Must_Return_Null_if_Id_To_Edit_Is_Not_Found ()
+        {
+            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
+            Assert.IsNull(await _sut.Edit(new("", "", "", "")));
+        }
+
+
+        [TestMethod]
+        public async Task Must_Return_Null_if_Data_Provided_To_Edit_Is_Invalid ()
+        {
+            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church("valid_id",
+                "valid_name", new("valid_email@email.com", "valid_password")));
+            Assert.IsNull(await _sut.Edit(new("", "", "", "")));
+        }
+
+        [TestMethod]
+        public async Task Must_Add_Notifications_if_Data_Provided_To_Edit_Is_Invalid ()
+        {
+            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church("valid_id",
+                "valid_name", new("valid_email@email.com", "valid_password")));
+            await _sut.Edit(new(Guid.NewGuid().ToString(), "", "", ""));
+            var messages = _sut.Notificator.Notifications
+                .Select(notification => $"{notification.Key} - {notification.Message}").ToArray();
+            Assert.IsTrue(_sut.Notificator.HasNotifications);
+            Assert.AreEqual(5, messages.Length);
+            Assert.AreEqual(messages[0], "Email - Este campo deve ser informado(a).");
+            Assert.AreEqual(messages[1], "Email - Forneça um email válido.");
+            Assert.AreEqual(messages[2], "Password - Este campo deve ser informado(a).");
+            Assert.AreEqual(messages[3], "Password - Este campo deve ser maior que 8 caracteres.");
+            Assert.AreEqual(messages[4], "Name - Este campo deve ser informado(a).");
+        }
+
+        [TestMethod]
+        public async Task Must_Return_Null_if_Entity_is_Not_Persisted_In_Edit_Method ()
+        {
+            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church("valid_id",
+                "valid_name", new("valid_email@email.com", "valid_password")));
+            _repository.Setup(repository => repository.Update(It.IsAny<Church>())).ReturnsAsync(false);
+            Assert.IsNull(await _sut.Edit(new(Guid.NewGuid().ToString(), "valid_name", "valid_email@email.com",
+                "valid_password")));
+        }
+
+        [TestMethod]
+        public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted_In_Edit_Method ()
+        {
+            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church("valid_id",
+                "valid_name", new("valid_email@email.com", "valid_password")));
+            _repository.Setup(repository => repository.Update(It.IsAny<Church>())).ReturnsAsync(true);
+            var data = new EditChurchRequest(Guid.NewGuid().ToString(), "valid_name", "valid_email@email.com",
+                "valid_password");
+            var response = await _sut.Edit(data);
+            Assert.IsInstanceOfType(response, typeof(EditChurchResponse));
+            Assert.IsNotNull(response.Id);
+            Assert.AreEqual(data.Name, response.Name);
+            Assert.AreEqual(data.Email, response.Email);
+        }
+
+        [TestMethod]
+        public async Task Must_Return_Null_if_Id_To_Delete_Is_Not_Found ()
+        {
+            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
+            Assert.IsNull(await _sut.Delete(new("")));
+        }
+
+
+        [TestMethod]
+        public async Task Must_Return_Response_if_Entity_Is_Deleted ()
+        {
+            _repository.Setup(repository => repository.Remove(It.IsAny<string>())).ReturnsAsync(true);
+            Assert.IsNotNull(await _sut.Delete(new("valid_id")));
         }
     }
 }
