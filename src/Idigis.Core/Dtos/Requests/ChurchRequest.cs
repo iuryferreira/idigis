a756db7b484c4baacf742fa552241b7c77b4bffe
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-06 21:32:48 -0300

    ✨ add edit and delete dtos

--- src/Idigis.Core/Dtos/Requests/ChurchRequest.cs
+++ src/Idigis.Core/Dtos/Requests/ChurchRequest.cs
@@ -13,4 +13,30 @@ namespace Idigis.Core.Dtos.Requests
         public string Email { get; }
         public string Password { get; }
     }
+
+    public class EditChurchRequest
+    {
+        public EditChurchRequest (string id, string name, string email, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Password { get; }
+    }
+
+    public class DeleteChurchRequest
+    {
+        public DeleteChurchRequest (string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+    }
 }
--- src/Idigis.Core/Dtos/Responses/ChurchResponse.cs
+++ src/Idigis.Core/Dtos/Responses/ChurchResponse.cs
@@ -6,4 +6,15 @@ namespace Idigis.Core.Dtos.Responses
         public string Name { get; init; }
         public string Email { get; init; }
     }
+
+    public class EditChurchResponse
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Email { get; init; }
+    }
+
+    public class DeleteChurchResponse
+    {
+    }
 }
