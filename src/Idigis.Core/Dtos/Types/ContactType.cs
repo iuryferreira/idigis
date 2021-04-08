9c6331ba934f1b57e91893f766a86baa961105e0
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-08 13:57:36 -0300

    ✨ add dtos

---
+++ src/Idigis.Core/Dtos/Types/ContactType.cs
@@ -0,0 +1,20 @@
+namespace Idigis.Core.Dtos.Types
+{
+    public class ContactType
+    {
+        public ContactType (string phoneNumber, string houseNumber, string street, string district, string city)
+        {
+            PhoneNumber = phoneNumber;
+            HouseNumber = houseNumber;
+            Street = street;
+            District = district;
+            City = city;
+        }
+
+        public string PhoneNumber { get; }
+        public string HouseNumber { get; }
+        public string Street { get; }
+        public string District { get; }
+        public string City { get; }
+    }
+}
