e99c1384998641aa6e2eff20b2833b9bbd6b416f
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-08 19:52:16 -0300

    ✅  update tests

--- src/Idigis.Tests/UnitTests/UseCases/MemberUseCaseTest.cs
+++ src/Idigis.Tests/UnitTests/UseCases/MemberUseCaseTest.cs
@@ -17,7 +17,6 @@ namespace Idigis.Tests.UnitTests.UseCases
     [TestClass]
     public class MemberUseCaseTest
     {
-        private readonly Mock<IChurchRepository> _churchRepository;
         private readonly Mock<IMemberRepository> _repository;
         private AbstractNotificator _notificator;
         private IMemberUseCase _sut;
@@ -25,41 +24,22 @@ namespace Idigis.Tests.UnitTests.UseCases
         public MemberUseCaseTest ()
         {
             _repository = new();
-            _churchRepository = new();
         }
 
         [TestInitialize]
         public void BeforeEach ()
         {
             _notificator = new Notificator();
-            _sut = new MemberUseCase(_notificator, _repository.Object, _churchRepository.Object);
+            _sut = new MemberUseCase(_notificator, _repository.Object);
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Church_Is_Not_Found ()
+        public async Task Must_Return_Null_And_Add_Notifications_if_Data_Provided_To_Create_Is_Invalid ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
-            Assert.IsNull(await _sut.Add(new("valid_id", "any_name")));
-        }
-
-        [TestMethod]
-        public async Task Must_Return_Null_if_Data_Provided_To_Create_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            Assert.IsNull(await _sut.Add(new("valid_id", "any_name")));
-        }
-
-        [TestMethod]
-        public async Task Must_Add_Notifications_if_Data_Provided_To_Create_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            await _sut.Add(new("valid_id", ""));
+            var response = await _sut.Add(new("valid_id", ""));
             var messages = _sut.Notificator.Notifications
                 .Select(notification => $"{notification.Key} - {notification.Message}").ToArray();
+            Assert.IsNull(response);
             Assert.IsTrue(_sut.Notificator.HasNotifications);
             Assert.AreEqual(1, messages.Length);
             Assert.AreEqual(messages[0], "FullName - Este campo deve ser informado(a).");
@@ -68,20 +48,14 @@ namespace Idigis.Tests.UnitTests.UseCases
         [TestMethod]
         public async Task Must_Return_Null_if_Entity_is_Not_Persisted_In_Add_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.Add(It.IsAny<Member>())).ReturnsAsync(false);
+            _repository.Setup(repository => repository.Add(It.IsAny<string>(), It.IsAny<Member>())).ReturnsAsync(false);
             Assert.IsNull(await _sut.Add(new("valid_id", "any_name")));
         }
 
         [TestMethod]
         public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted_In_Add_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.Add(It.IsAny<Member>())).ReturnsAsync(true);
+            _repository.Setup(repository => repository.Add(It.IsAny<string>(), It.IsAny<Member>())).ReturnsAsync(true);
             var data = new CreateMemberRequest("valid_id", "any_name", new(2000, 10, 10), new(2012, 10, 10),
                 new("00000000000", "0", "any_street", "any_district", "any_city"));
             var response = await _sut.Add(data);
@@ -97,36 +71,13 @@ namespace Idigis.Tests.UnitTests.UseCases
             Assert.AreEqual(data.Contact.PhoneNumber, response.Contact.PhoneNumber);
         }
 
-
         [TestMethod]
-        public async Task Must_Return_Null_if_Church_To_Edit_Is_Not_Found ()
+        public async Task Must_Return_Null_And_Add_Notifications_if_Data_Provided_To_Edit_Is_Invalid ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
-            Assert.IsNull(await _sut.Edit(new("invalid_id", "valid_id", "valid_name")));
-        }
-
-        [TestMethod]
-        public async Task Must_Return_Null_if_Data_Provided_To_Edit_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>()))
-                .ReturnsAsync(new Member("any_name"));
-            Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), "")));
-        }
-
-        [TestMethod]
-        public async Task Must_Add_Notifications_if_Data_Provided_To_Edit_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>()))
-                .ReturnsAsync(new Member("any_name"));
-            Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), "")));
+            var response = await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), ""));
             var messages = _sut.Notificator.Notifications
                 .Select(notification => $"{notification.Key} - {notification.Message}").ToArray();
+            Assert.IsNull(response);
             Assert.IsTrue(_sut.Notificator.HasNotifications);
             Assert.AreEqual(1, messages.Length);
             Assert.AreEqual(messages[0], "FullName - Este campo deve ser informado(a).");
@@ -135,43 +86,23 @@ namespace Idigis.Tests.UnitTests.UseCases
         [TestMethod]
         public async Task Must_Return_Null_if_Entity_is_Not_Persisted_In_Edit_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>()))
-                .ReturnsAsync(new Member("any_name"));
-            _repository.Setup(repository => repository.Update(It.IsAny<Member>())).ReturnsAsync(false);
+            _repository.Setup(repository => repository.Update(It.IsAny<string>(), It.IsAny<Member>()))
+                .ReturnsAsync(false);
             Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), "valid_name")));
-        }
-
-        [TestMethod]
-        public async Task Must_Return_Null_if_Member_Is_Not_Found_In_Edit_Method ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Member)null);
-            Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), "any_name")));
         }
 
         [TestMethod]
         public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted_In_Edit_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(
-                new Church(
-                    "valid_id",
-                    "valid_name",
-                    new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>()))
-                .ReturnsAsync(new Member("any_name"));
-            _repository.Setup(repository => repository.Update(It.IsAny<Member>())).ReturnsAsync(true);
-            var data = new EditMemberRequest("valid_id", "any_name", "any_name",
+            _repository.Setup(repository => repository.Update(It.IsAny<string>(), It.IsAny<Member>()))
+                .ReturnsAsync(true);
+            var data = new EditMemberRequest("valid_id", Guid.NewGuid().ToString(), "any_name",
                 new(2000, 10, 10), new(2012, 10, 10),
                 new("00000000000", "0",
                     "any_street", "any_district", "any_city"));
             var response = await _sut.Edit(data);
             Assert.IsInstanceOfType(response, typeof(EditMemberResponse));
-            Assert.IsNotNull(response.Id);
+            Assert.AreEqual(data.Id, response.Id);
             Assert.AreEqual(data.FullName, response.FullName);
             Assert.AreEqual(data.BirthDate, response.BirthDate);
             Assert.AreEqual(data.BaptismDate, response.BaptismDate);
@@ -183,19 +114,10 @@ namespace Idigis.Tests.UnitTests.UseCases
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Church_To_Delete_Member_Is_Not_Found ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
-            Assert.IsNull(await _sut.Delete(new("invalid_id", "offer_id")));
-        }
-
-        [TestMethod]
         public async Task Must_Return_Null_if_Id_To_Delete_Is_Not_Found ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Member)null);
+            _repository.Setup(repository => repository.Remove(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(false);
             Assert.IsNull(await _sut.Delete(new("invalid_id", "invalid_id")));
         }
 
@@ -203,10 +125,8 @@ namespace Idigis.Tests.UnitTests.UseCases
         [TestMethod]
         public async Task Must_Return_Response_if_Entity_Is_Deleted ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.Remove(It.IsAny<string>())).ReturnsAsync(true);
+            _repository.Setup(repository => repository.Remove(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(true);
             Assert.IsNotNull(await _sut.Delete(new("valid_id", "valid_id")));
         }
     }
--- src/Idigis.Tests/UnitTests/UseCases/OfferUseCaseTest.cs
+++ src/Idigis.Tests/UnitTests/UseCases/OfferUseCaseTest.cs
@@ -17,7 +17,6 @@ namespace Idigis.Tests.UnitTests.UseCases
     [TestClass]
     public class OfferUseCaseTest
     {
-        private readonly Mock<IChurchRepository> _churchRepository;
         private readonly Mock<IOfferRepository> _repository;
         private AbstractNotificator _notificator;
         private IOfferUseCase _sut;
@@ -25,41 +24,22 @@ namespace Idigis.Tests.UnitTests.UseCases
         public OfferUseCaseTest ()
         {
             _repository = new();
-            _churchRepository = new();
         }
 
         [TestInitialize]
         public void BeforeEach ()
         {
             _notificator = new Notificator();
-            _sut = new OfferUseCase(_notificator, _repository.Object, _churchRepository.Object);
+            _sut = new OfferUseCase(_notificator, _repository.Object);
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Church_Is_Not_Found ()
+        public async Task Must_Return_Null_And_Add_Notifications_if_Data_Provided_To_Create_Is_Invalid ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
-            Assert.IsNull(await _sut.Add(new("invalid_id", 0)));
-        }
-
-        [TestMethod]
-        public async Task Must_Return_Null_if_Data_Provided_To_Create_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            Assert.IsNull(await _sut.Add(new("", 0)));
-        }
-
-        [TestMethod]
-        public async Task Must_Add_Notifications_if_Data_Provided_To_Create_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            await _sut.Add(new("valid_id", 0));
+            var response = await _sut.Add(new("any_id", 0));
             var messages = _sut.Notificator.Notifications
                 .Select(notification => $"{notification.Key} - {notification.Message}").ToArray();
+            Assert.IsNull(response);
             Assert.IsTrue(_sut.Notificator.HasNotifications);
             Assert.AreEqual(1, messages.Length);
             Assert.AreEqual(messages[0], "Value - Este campo deve ser maior que 0.");
@@ -68,20 +48,14 @@ namespace Idigis.Tests.UnitTests.UseCases
         [TestMethod]
         public async Task Must_Return_Null_if_Entity_is_Not_Persisted_In_Add_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.Add(It.IsAny<Offer>())).ReturnsAsync(false);
+            _repository.Setup(repository => repository.Add(It.IsAny<string>(), It.IsAny<Offer>())).ReturnsAsync(false);
             Assert.IsNull(await _sut.Add(new("valid_id", 1)));
         }
 
         [TestMethod]
         public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted_In_Add_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.Add(It.IsAny<Offer>())).ReturnsAsync(true);
+            _repository.Setup(repository => repository.Add(It.IsAny<string>(), It.IsAny<Offer>())).ReturnsAsync(true);
             var data = new CreateOfferRequest("valid_id", 1);
             var response = await _sut.Add(data);
             Assert.IsInstanceOfType(response, typeof(CreateOfferResponse));
@@ -90,32 +64,12 @@ namespace Idigis.Tests.UnitTests.UseCases
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Church_To_Edit_Is_Not_Found ()
+        public async Task Must_Return_Null_And_Add_Notifications_if_Data_Provided_To_Edit_Is_Invalid ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
-            Assert.IsNull(await _sut.Edit(new("invalid_id", "offer_id", (decimal)32.1)));
-        }
-
-        [TestMethod]
-        public async Task Must_Return_Null_if_Data_Provided_To_Edit_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Offer(3));
-            Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), 0)));
-        }
-
-        [TestMethod]
-        public async Task Must_Add_Notifications_if_Data_Provided_To_Edit_Is_Invalid ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Offer(3));
-            await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), 0));
+            var response = await _sut.Edit(new("any_id", Guid.NewGuid().ToString(), 0));
             var messages = _sut.Notificator.Notifications
                 .Select(notification => $"{notification.Key} - {notification.Message}").ToArray();
+            Assert.IsNull(response);
             Assert.IsTrue(_sut.Notificator.HasNotifications);
             Assert.AreEqual(1, messages.Length);
             Assert.AreEqual(messages[0], "Value - Este campo deve ser maior que 0.");
@@ -124,33 +78,16 @@ namespace Idigis.Tests.UnitTests.UseCases
         [TestMethod]
         public async Task Must_Return_Null_if_Entity_is_Not_Persisted_In_Edit_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Offer(3));
-            _repository.Setup(repository => repository.Update(It.IsAny<Offer>())).ReturnsAsync(false);
-            Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), 30)));
-        }
-
-        [TestMethod]
-        public async Task Must_Return_Null_if_Offer_Is_Not_Found_In_Edit_Method ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Offer)null);
+            _repository.Setup(repository => repository.Update(It.IsAny<string>(), It.IsAny<Offer>()))
+                .ReturnsAsync(false);
             Assert.IsNull(await _sut.Edit(new("valid_id", Guid.NewGuid().ToString(), 30)));
         }
 
         [TestMethod]
         public async Task Must_Return_a_Response_If_the_Entity_Is_Valid_And_Persisted_In_Edit_Method ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name",
-                new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Offer(3));
-            _repository.Setup(repository => repository.Update(It.IsAny<Offer>())).ReturnsAsync(true);
+            _repository.Setup(repository => repository.Update(It.IsAny<string>(), It.IsAny<Offer>()))
+                .ReturnsAsync(true);
             var data = new EditOfferRequest("valid_id", Guid.NewGuid().ToString(), 3);
             var response = await _sut.Edit(data);
             Assert.IsInstanceOfType(response, typeof(EditOfferResponse));
@@ -159,19 +96,10 @@ namespace Idigis.Tests.UnitTests.UseCases
         }
 
         [TestMethod]
-        public async Task Must_Return_Null_if_Church_To_Delete_Offer_Is_Not_Found ()
-        {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Church)null);
-            Assert.IsNull(await _sut.Delete(new("invalid_id", "offer_id")));
-        }
-
-        [TestMethod]
         public async Task Must_Return_Null_if_Id_To_Delete_Is_Not_Found ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync((Offer)null);
+            _repository.Setup(repository => repository.Remove(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(false);
             Assert.IsNull(await _sut.Delete(new("invalid_id", "invalid_id")));
         }
 
@@ -179,10 +107,8 @@ namespace Idigis.Tests.UnitTests.UseCases
         [TestMethod]
         public async Task Must_Return_Response_if_Entity_Is_Deleted ()
         {
-            _churchRepository.Setup(repository => repository.GetById(It.IsAny<string>())).ReturnsAsync(new Church(
-                "valid_id",
-                "valid_name", new("valid_email@email.com", "valid_password")));
-            _repository.Setup(repository => repository.Remove(It.IsAny<string>())).ReturnsAsync(true);
+            _repository.Setup(repository => repository.Remove(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(true);
             Assert.IsNotNull(await _sut.Delete(new("valid_id", "valid_id")));
         }
     }
