211f6948ce1d13aa5e9933782e96c58ccb24fe56
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-10 15:54:04 -0300

    ✨ add notificator to contract

--- src/Idigis.Core/Persistence/Contracts/IChurchRepository.cs
+++ src/Idigis.Core/Persistence/Contracts/IChurchRepository.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using Idigis.Core.Domain.Entities;
+using Notie.Contracts;
 
 namespace Idigis.Core.Persistence.Contracts
 {
     internal interface IChurchRepository
     {
+        AbstractNotificator Notificator { get; }
         Task<bool> Add (Church entity);
         Task<Church> GetById (string id);
         Task<Church> GetByEmail (string email);
