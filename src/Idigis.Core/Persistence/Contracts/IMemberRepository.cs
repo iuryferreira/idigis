1bc941e36708d4fec2b3a9bb7b3f746004a185fd
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-10 17:10:58 -0300

    ✨ add notificator

--- src/Idigis.Core/Persistence/Contracts/IMemberRepository.cs
+++ src/Idigis.Core/Persistence/Contracts/IMemberRepository.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Idigis.Core.Domain.Entities;
+using Notie.Contracts;
 
 namespace Idigis.Core.Persistence.Contracts
 {
     internal interface IMemberRepository
     {
+        AbstractNotificator Notificator { get; }
         Task<bool> Add (string churchId, Member entity);
         Task<Member> GetById (string churchId, string id);
         Task<List<Member>> All (string churchId);
--- src/Idigis.Core/Persistence/Contracts/ITitheRepository.cs
+++ src/Idigis.Core/Persistence/Contracts/ITitheRepository.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Idigis.Core.Domain.Entities;
+using Notie.Contracts;
 
 namespace Idigis.Core.Persistence.Contracts
 {
     internal interface ITitheRepository
     {
+        AbstractNotificator Notificator { get; }
         Task<bool> Add (string churchId, string memberId, Tithe entity);
         Task<Tithe> GetById (string churchId, string memberId, string id);
         Task<List<Tithe>> All (string churchId, string memberId);
