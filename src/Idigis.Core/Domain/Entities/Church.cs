124961b6fda1698bccfa001b3918833346787d51
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-10 15:53:46 -0300

    🐛 fix re-hashing in second constructor

--- src/Idigis.Core/Domain/Entities/Church.cs
+++ src/Idigis.Core/Domain/Entities/Church.cs
@@ -23,10 +23,7 @@ namespace Idigis.Core.Domain.Entities
             Id = id;
             Name = name;
             Credentials = credentials;
-            if (Validate(this, new ChurchValidator()))
-            {
-                Credentials.Password = new Hashio().Hash(Credentials.Password);
-            }
+            Validate(this, new ChurchValidator());
         }
 
         internal string Name { get; }
