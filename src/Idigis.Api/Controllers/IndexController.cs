46e357c8e3eb1ec6b0d8c071e88b7f6462af702e
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-11 09:50:31 -0300

    ✨ add signup method

---
+++ src/Idigis.Api/Controllers/IndexController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Idigis.Core.Application.Contracts;
+using Idigis.Shared.Dtos.Requests;
+using Idigis.Shared.Dtos.Responses;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Idigis.Api.Controllers
+{
+    [Route(Routes.Base)]
+    public class IndexController : ControllerBase
+    {
+        private readonly IChurchUseCase _churchUseCase;
+
+        public IndexController (IChurchUseCase churchUseCase)
+        {
+            _churchUseCase = churchUseCase;
+        }
+
+        [HttpPost]
+        [Route("signup")]
+        public async Task<ActionResult<CreateChurchResponse>> Signup ([FromBody] CreateChurchRequest request)
+        {
+            var response = await _churchUseCase.Add(request);
+            if (response is not null)
+            {
+                return Created($"{Request.GetDisplayUrl()}/{response.Id}", response);
+            }
+
+            return BadRequest(_churchUseCase.Notificator.Notifications);
+        }
+    }
+}
