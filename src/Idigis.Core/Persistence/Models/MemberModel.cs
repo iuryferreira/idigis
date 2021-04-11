562e1b7baa8a3f84f2d2ebfe581051b01c0a52ab
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-11 00:34:51 -0300

    ✨ add member repository and model

---
+++ src/Idigis.Core/Persistence/Models/MemberModel.cs
@@ -0,0 +1,50 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Idigis.Core.Domain.Entities;
+using Idigis.Core.Persistence.Contracts;
+
+namespace Idigis.Core.Persistence.Models
+{
+    [Table("members")]
+    internal class MemberModel : Model
+    {
+        [Required] public string FullName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? BaptismDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HouseNumber { get; set; }
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+
+        [Required] public string ChurchId { get; set; }
+        [Required] public ChurchModel Church { get; set; }
+
+        public static implicit operator MemberModel (Member entity)
+        {
+            if (entity is null) { return null; }
+
+            return new()
+            {
+                Id = entity.Id,
+                FullName = entity.FullName,
+                BirthDate = entity.BirthDate,
+                BaptismDate = entity.BaptismDate,
+                City = entity.Contact?.City,
+                District = entity.Contact?.District,
+                PhoneNumber = entity.Contact?.PhoneNumber,
+                Street = entity.Contact?.Street,
+                HouseNumber = entity.Contact?.HouseNumber
+            };
+        }
+
+        public static implicit operator Member (MemberModel model)
+        {
+            if (model is null) { return null; }
+
+            return new(model.FullName, model.BirthDate, model.BirthDate,
+                new(model.PhoneNumber, model.HouseNumber, model.Street, model.District, model.City));
+        }
+    }
+}
---
+++ src/Idigis.Core/Persistence/Repositories/MemberRepository.cs
@@ -0,0 +1,173 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Idigis.Core.Domain.Entities;
+using Idigis.Core.Persistence.Contracts;
+using Idigis.Core.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using Notie.Contracts;
+
+namespace Idigis.Core.Persistence.Repositories
+{
+    internal class MemberRepository : IMemberRepository
+    {
+        private readonly Context _context;
+
+        public MemberRepository (AbstractNotificator notificator, Context context)
+        {
+            Notificator = notificator;
+            _context = context;
+        }
+
+        public AbstractNotificator Notificator { get; }
+
+        public async Task<bool> Add (string churchId, Member entity)
+        {
+            MemberModel model = entity;
+            try
+            {
+                var churchExists =
+                    await _context.ChurchContext.FirstOrDefaultAsync(church => church.Id == churchId) is not null;
+                if (!churchExists)
+                {
+                    Notificator.SetNotificationType(new("Validation"));
+                    Notificator.AddNotification(new("Repository", "Esta igreja não existe no sistema."));
+                    return false;
+                }
+
+                model.ChurchId = churchId;
+                var entry = await _context.MemberContext.AddAsync(model);
+                await _context.SaveChangesAsync();
+                return entry.State is EntityState.Unchanged;
+            }
+            catch
+            {
+                Notificator.AddNotification(new("Repository", "Ocorreu um erro na inserção."));
+                return false;
+            }
+        }
+
+        public async Task<Member> GetById (string churchId, string id)
+        {
+            try
+            {
+                var members = await _context.ChurchContext.Where(c => c.Id == churchId)
+                    .Select(c => c.Members)
+                    .FirstOrDefaultAsync();
+                if (members is null)
+                {
+                    Notificator.SetNotificationType(new("Validation"));
+                    Notificator.AddNotification(new("Repository", "Esta igreja não existe no sistema."));
+                    return null;
+                }
+
+                var model = members.Find(member => member.Id == id);
+                if (model is null)
+                {
+                    Notificator.AddNotification(new("Repository", "Registro não encontrado."));
+                }
+
+                return model;
+            }
+            catch
+            {
+                Notificator.AddNotification(new("Repository", "Ocorreu um erro na busca."));
+                return null;
+            }
+        }
+
+        public async Task<List<Member>> All (string churchId)
+        {
+            try
+            {
+                var members = await _context.ChurchContext.Where(c => c.Id == churchId)
+                    .Select(c => c.Members.Select(o => (Member)o).ToList())
+                    .FirstOrDefaultAsync();
+                if (members is not null)
+                {
+                    return members;
+                }
+
+                Notificator.SetNotificationType(new("Validation"));
+                Notificator.AddNotification(new("Repository", "Esta igreja não existe no sistema."));
+                return null;
+            }
+            catch
+            {
+                Notificator.AddNotification(new("Repository", "Ocorreu um erro na listagem."));
+                return null;
+            }
+        }
+
+        public async Task<bool> Update (string churchId, Member entity)
+        {
+            try
+            {
+                var members = await _context.ChurchContext.Where(c => c.Id == churchId)
+                    .Select(c => c.Members)
+                    .FirstOrDefaultAsync();
+                if (members is null)
+                {
+                    Notificator.SetNotificationType(new("Validation"));
+                    Notificator.AddNotification(new("Repository", "Esta igreja não existe no sistema."));
+                    return false;
+                }
+
+                var model = members.Find(member => member.Id == entity.Id);
+                if (model is null)
+                {
+                    Notificator.AddNotification(new("Repository", "Registro não encontrado."));
+                    return false;
+                }
+
+                model.FullName = entity.FullName;
+                model.BirthDate = entity.BirthDate;
+                model.BaptismDate = entity.BaptismDate;
+                model.PhoneNumber = entity.Contact?.PhoneNumber;
+                model.HouseNumber = entity.Contact?.HouseNumber;
+                model.Street = entity.Contact?.Street;
+                model.District = entity.Contact?.District;
+                model.City = entity.Contact?.City;
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                Notificator.AddNotification(new("Repository", "Ocorreu um erro na atualização."));
+                return false;
+            }
+        }
+
+        public async Task<bool> Remove (string churchId, string id)
+        {
+            try
+            {
+                var members = await _context.ChurchContext.Where(c => c.Id == churchId)
+                    .Select(c => c.Members)
+                    .FirstOrDefaultAsync();
+                if (members is null)
+                {
+                    Notificator.SetNotificationType(new("Validation"));
+                    Notificator.AddNotification(new("Repository", "Esta igreja não existe no sistema."));
+                    return false;
+                }
+
+                var model = members.Find(member => member.Id == id);
+                if (model is null)
+                {
+                    Notificator.AddNotification(new("Repository", "Registro não encontrado."));
+                    return false;
+                }
+
+                _context.MemberContext.Remove(model);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                Notificator.AddNotification(new("Repository", "Ocorreu um erro na remoção."));
+                return false;
+            }
+        }
+    }
+}
