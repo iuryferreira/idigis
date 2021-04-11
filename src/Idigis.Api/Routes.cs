78efeeb95dca942be5d2d73e28fa6f367b72bd9e
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-11 12:33:47 -0300

    ✨ add church routes

--- src/Idigis.Api/Routes.cs
+++ src/Idigis.Api/Routes.cs
@@ -2,11 +2,15 @@ namespace Idigis.Api
 {
     public static class Routes
     {
-        public const string Base = "api/";
-
         public static class Index
         {
+            public const string Base = "api/";
             public const string Signup = Base + "signup";
+        }
+
+        public static class Church
+        {
+            public const string Base = "api/church/";
         }
     }
 }
