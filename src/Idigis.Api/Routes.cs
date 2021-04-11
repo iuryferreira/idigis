b0d3d6a676efd80c3b89446cc399b9b2d8b58453
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-11 09:36:23 -0300

    ✨ add routes path

---
+++ src/Idigis.Api/Routes.cs
@@ -0,0 +1,12 @@
+namespace Idigis.Api
+{
+    public static class Routes
+    {
+        public const string Base = "api/";
+
+        public static class Index
+        {
+            public const string Signup = Base + "signup";
+        }
+    }
+}
