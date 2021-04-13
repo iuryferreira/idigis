2df6a2bd84a359e06cc984129e7adfd0fcf9f242
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-13 17:44:18 -0300

    ✨ add signin controller method

--- src/Idigis.Api/Controllers/IndexController.cs
+++ src/Idigis.Api/Controllers/IndexController.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Idigis.Api.Auth.Contracts;
 using Idigis.Core.Application.Contracts;
 using Idigis.Shared.Dtos.Requests;
 using Idigis.Shared.Dtos.Responses;
@@ -10,24 +11,57 @@ namespace Idigis.Api.Controllers
     [Route(Routes.Index.Base)]
     public class IndexController : ControllerBase
     {
-        private readonly IChurchUseCase _churchUseCase;
+        private readonly IChurchUseCase _usecase;
+        private readonly IAuthService _authService;
 
-        public IndexController (IChurchUseCase churchUseCase)
+        public IndexController (IChurchUseCase usecase, IAuthService authService)
         {
-            _churchUseCase = churchUseCase;
+            _usecase = usecase;
+            _authService = authService;
         }
-
+        
         [HttpPost]
         [Route("signup")]
         public async Task<ActionResult<CreateChurchResponse>> Signup ([FromBody] CreateChurchRequest request)
         {
-            var response = await _churchUseCase.Add(request);
+            var response = await _usecase.Add(request);
             if (response is not null)
             {
                 return Created($"{Request.GetDisplayUrl()}/{response.Id}", response);
             }
 
-            return BadRequest(_churchUseCase.Notificator.Notifications);
+            return BadRequest(_usecase.Notificator.Notifications);
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public async Task<ActionResult<LoginResponse>> Signin ([FromBody] LoginRequest request)
+        {
+            
+            var church = await _usecase.Get(new(request.Email));
+            if (church is null)
+            {
+                return _usecase.Notificator.NotificationType.Name switch
+                {
+                    "NotFound" => NotFound(_usecase.Notificator.Notifications),
+                    _ => StatusCode(500, _usecase.Notificator.Notifications)
+                };
+            }
+
+            request.Hash = church.Password;
+            request.ChurchId = church.Id;
+            request.Name = church.Name;
+            var response = _authService.Authenticate(request);
+            if (response is not null)
+            {
+                return Ok(response);
+            }
+            return _usecase.Notificator.NotificationType.Name switch
+            {
+                "Unauthorized" => Unauthorized(_usecase.Notificator.Notifications),
+                _ => StatusCode(500, _usecase.Notificator.Notifications)
+            };
+
         }
     }
 }
