4bf8c8d2feae8fa946a73ef8bdfb4ea2d96d5f89
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-13 17:42:30 -0300

    ✨ add dtos to signin

---
+++ src/Idigis.Shared/Dtos/Requests/AuthRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Idigis.Shared.Dtos.Requests
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Hash { get; set; }
+        public string ChurchId { get; set; }
+        public string Name { get; set; }
+    }
+}
