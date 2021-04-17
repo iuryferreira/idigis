56aae4a5d282b85805dea37ff66de24e129469f2
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-16 23:05:04 -0300

    ♻️ remove try catch

--- src/Idigis.Api/Auth/AuthService.cs
+++ src/Idigis.Api/Auth/AuthService.cs
@@ -7,7 +7,6 @@ using Idigis.Shared.Dtos.Requests;
 using Idigis.Shared.Dtos.Responses;
 using Microsoft.IdentityModel.Tokens;
 using Notie.Contracts;
-using Notie.Models;
 
 namespace Idigis.Api.Auth
 {
@@ -40,30 +39,21 @@ namespace Idigis.Api.Auth
         public string GenerateToken (string email, string id)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            try
+            var tokenDescriptor = new SecurityTokenDescriptor
             {
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject =
-                        new(new[]
-                        {
+                Subject =
+                    new(new[]
+                    {
                             new(ClaimTypes.Email, email), new Claim(ClaimTypes.PrimarySid, id),
                             new(ClaimTypes.Uri, AuthSettings.ServerUrl)
-                        }),
-                    Expires =
-                        DateTime.UtcNow.AddHours(double.Parse(Environment.GetEnvironmentVariable("JwtExpirationInHours") ?? string.Empty)),
-                    SigningCredentials =
-                        new(new SymmetricSecurityKey(AuthSettings.Key), SecurityAlgorithms.HmacSha256Signature)
-                };
-                var token = tokenHandler.CreateToken(tokenDescriptor);
-                return tokenHandler.WriteToken(token);
-            }
-            catch
-            {
-                Notificator.SetNotificationType(new("Internal"));
-                Notificator.AddNotification(new("Auth", "Ocorreu um erro na geração do acesso."));
-                return "";
-            }
+                    }),
+                Expires =
+                    DateTime.UtcNow.AddHours(double.Parse(Environment.GetEnvironmentVariable("JwtExpirationInHours") ?? string.Empty)),
+                SigningCredentials =
+                    new(new SymmetricSecurityKey(AuthSettings.Key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
