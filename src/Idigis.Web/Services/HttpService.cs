6ecf197ab13a9c29a7920c7380ddb51e552535bb
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-18 20:18:31 -0300

    ✨ add http service

---
+++ src/Idigis.Web/Services/HttpService.cs
@@ -0,0 +1,48 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Idigis.Web.Helpers;
+
+namespace Idigis.Web.Services
+{
+    public interface IHttpService
+    {
+        Task<HttpResponseMessage> Post<T> (string uri, T data);
+        Task<HttpResponseMessage> Get (string uri);
+    }
+
+    public class HttpService : IHttpService
+    {
+        private readonly HttpClient _http;
+        private readonly ILocalStorageService _localStorageService;
+
+        public HttpService (HttpClient http, ILocalStorageService localStorageService)
+        {
+            _http = http;
+            _localStorageService = localStorageService;
+        }
+
+        public async Task<HttpResponseMessage> Get (string uri)
+        {
+            await HasToken();
+            return await _http.GetAsync(uri);
+        }
+
+        public async Task<HttpResponseMessage> Post<T> (string uri, T data)
+        {
+            await HasToken();
+            return await _http.PostAsJsonAsync(uri, data);
+        }
+
+        private async Task HasToken ()
+        {
+            var token = await _localStorageService.GetItemAsync<string>("token");
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                var decoded = CustomEncoder.Decode<string>(token);
+                _http.DefaultRequestHeaders.Authorization = new("Bearer", decoded);
+            }
+        }
+    }
+}
