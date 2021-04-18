45998782901908b362629e1c0c9bc2156748434e
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-18 20:17:45 -0300

    ✨ add church and error models

---
+++ src/Idigis.Web/Models/Church.cs
@@ -0,0 +1,10 @@
+namespace Idigis.Web.Models
+{
+    public class Church
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ src/Idigis.Web/Models/Error.cs
@@ -0,0 +1,8 @@
+namespace Idigis.Web.Models
+{
+    public class Error
+    {
+        public string Key { get; init; }
+        public string Message { get; init; }
+    }
+}
