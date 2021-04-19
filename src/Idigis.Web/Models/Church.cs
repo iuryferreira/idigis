dc80373dbd9f3a7e8945da5b7be36e57e53a1ffc
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-19 16:47:27 -0300

    🔥 remove token in model

--- src/Idigis.Web/Models/Church.cs
+++ src/Idigis.Web/Models/Church.cs
@@ -5,6 +5,5 @@ namespace Idigis.Web.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Token { get; set; }
     }
 }
