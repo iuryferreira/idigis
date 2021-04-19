fd3874d668d8c57fa2d2ee5f0cef255342049214
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-18 22:29:38 -0300

    ✨ add signin api route

---
+++ src/Idigis.Web/ApiRoutes.cs
@@ -0,0 +1,10 @@
+namespace Idigis.Web
+{
+    public static class ApiRoutes
+    {
+        public static class Church
+        {
+            public const string Signin = "/signin";
+        }
+    }
+}
