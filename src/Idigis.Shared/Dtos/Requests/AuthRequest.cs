52f1103e81ca1bb6fc36dc73d8f43a5f4b1f857b
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-18 22:29:05 -0300

    ✨ add validation

--- src/Idigis.Shared/Dtos/Requests/AuthRequest.cs
+++ src/Idigis.Shared/Dtos/Requests/AuthRequest.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Idigis.Shared.Dtos.Requests
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email precisa ser válido.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MinLength(8, ErrorMessage = "A senha precisa ser maior que 8 caracteres.")]
         public string Password { get; set; }
         public string Hash { get; set; }
         public string ChurchId { get; set; }
