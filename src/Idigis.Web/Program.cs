49c65f64fdb9ddb06043f6538647b33e5678f450
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-04-19 16:40:03 -0300

    ✨ add blazor state and import

--- src/Idigis.Web/Program.cs
+++ src/Idigis.Web/Program.cs
@@ -1,7 +1,9 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
+using BlazorState;
 using Idigis.Web.Services;
+using Idigis.Web.States;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,7 +19,10 @@ namespace Idigis.Web
             builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
             builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new("https://localhost:5004") });
             builder.Services.AddScoped<IHttpService, HttpService>();
-            builder.Services.AddScoped<IChurchService, ChurchService>();
+            builder.Services.AddBlazorState(options =>
+            {
+                options.Assemblies = new[] { typeof(Program).Assembly };
+            });
             await builder.Build().RunAsync();
         }
     }
